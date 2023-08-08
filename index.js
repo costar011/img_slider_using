ce5ff0f02a00556a5ccb02d8bdceeff342e8be89
costar011 <dnaer456@gmail.com>
2023-08-08 23:46:26 +0900

    js

---
+++ index.js
@@ -0,0 +1,5 @@
+const initSlider = ($ele) => {
+    const sliderContainer = document.querySelector(`#${$ele}`);
+    const slide = document.querySelector(`#${$ele} #slide`);
+    const items = document.querySelectorAll(`#${$ele} #slide .item`);
+}
