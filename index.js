3265ed3fefd2bf69120e64dde3930ba0f33f4421
costar011 <dnaer456@gmail.com>
2023-08-09 23:20:34 +0900

    js

--- index.js
+++ index.js
@@ -2,4 +2,18 @@ const initSlider = ($ele) => {
     const sliderContainer = document.querySelector(`#${$ele}`);
     const slide = document.querySelector(`#${$ele} #slide`);
     const items = document.querySelectorAll(`#${$ele} #slide .item`);
-}
+    const next = document.querySelector(`#${$ele} .nextBtn`);
+    const prev = document.querySelector(`#${$ele} .prevBtn`);
+
+    let currentSlide = 0;
+
+    items[currentSlide].classList.add("activeSlide");
+    let itemWidth = items[0].clientWidth;
+
+
+    sliderContainer.style.width = itemWidth + "px";
+    sliderContainer.style.width = items.length * itemWidth + "px";
+    slide.style.transform = "translateY(0px)";
+
+    const toggleClass = (condition, elem, className);
+};
